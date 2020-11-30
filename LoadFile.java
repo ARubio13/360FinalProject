5ddcd3b1d248dea584c1a1167431601fabaed811
Adan Rubio <70557678+ARubio13@users.noreply.github.com>
2020-11-30 10:30:00 -0700

    Update LoadFile.java

--- LoadFile.java
+++ LoadFile.java
@@ -2,28 +2,18 @@ package AttendanceRoster;
 
 import java.io.File;
 import javax.swing.*;
-import java.util.Scanner;
 
 public class LoadFile {
     JFileChooser fc = new JFileChooser();
-    StringBuilder sb = new StringBuilder();
-    final String nl = "\n";
+
 
     public File pickFile() throws Exception {
         File file;
         
         if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                 file = fc.getSelectedFile();
-                Scanner in = new Scanner(file);
-
-                while(in.hasNext()) {
-                    sb.append(in.nextLine());
-                    sb.append(nl);
-                }
-                in.close();
             } else {
-                sb.append("No file loaded");
-            return null;
+                return null;
             }
         return file;
     }
