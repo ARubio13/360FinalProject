8a1a324497ae567de52f4268b6fd901e3e4689e1
Adan Rubio <70557678+ARubio13@users.noreply.github.com>
2020-11-30 12:56:49 -0700

    Updated to filter File Type

--- LoadFile.java
+++ LoadFile.java
@@ -1,14 +1,17 @@
 package AttendanceRoster;
 
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import javax.swing.*;
 
 public class LoadFile {
+    FileNameExtensionFilter filter = new FileNameExtensionFilter("CSV File", "csv");
     JFileChooser fc = new JFileChooser();
+    File file;
 
 
     public File pickFile() throws Exception {
-        File file;
+        fc.setFileFilter(filter);
         
         if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                 file = fc.getSelectedFile();
